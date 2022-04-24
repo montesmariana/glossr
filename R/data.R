d4188c5036a83986037804dc0aea429aa61cf55e
mariana <mariana.montes@kuleuven.be>
2022-04-24 21:23:30 +0200

    add examples for vignettes

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Examples of glosses
+#'
+#' A dataset containing five glossing examples extracted from Koptjevskaja-Tamm (2015)'s
+#' \emph{The Linguistics of Temperature} and chapters within.
+#'
+#' @format A \code{tibble} with 5 rows and 6 variables:
+#' \describe{
+#'   \item{original}{The text in the original language.}
+#'   \item{parsed}{The text with translations to English or morphological annotation per word or expression,
+#'   with LaTeX formatting.}
+#'   \item{translation}{Free translation to English.}
+#'   \item{label}{Label for referencing the example.}
+#'   \item{language}{Original language of the text.}
+#'   \item{Source}{Where the example was taken from (published paper).}
+#' }
+"glosses"
