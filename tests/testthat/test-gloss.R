773cf0471aed2636bfce8145cbe5e6af45866cdd
mariana <mariana.montes@kuleuven.be>
2022-05-07 20:47:53 +0200

    update documentation of as_gloss and gloss_data class

--- tests/testthat/test-gloss.R
+++ tests/testthat/test-gloss.R
@@ -3,10 +3,11 @@ ex_gloss <- "DET.M.SG example in Spanish"
 ex_third <- "One example in Spanish"
 ex_trans <- "An example in Spanish."
 
-bare <- create_gloss(ex_sp, ex_gloss, ex_third)
+bare <- new_gloss_data(list(ex_sp, ex_gloss, ex_third))
 
 test_that("class is correct", {
   expect_s3_class(bare, "gloss_data")
+  expect_s3_class(as_gloss(ex_sp, ex_gloss, translation = ex_trans), "gloss")
 })
 
 test_that("Default values are empty", {
@@ -18,8 +19,14 @@ test_that("Default values are empty", {
 })
 
 test_that("Translation is quoted", {
-  with_trans <- create_gloss(ex_sp, ex_gloss, translation = ex_trans)
+  with_trans <- new_gloss_data(list(ex_sp, ex_gloss), translation = ex_trans)
   expect_match(attr(with_trans, "translation"), "^\\\"An example in Spanish.\\\"$")
-  with_single <- create_gloss(ex_sp, ex_gloss, translation = ex_trans, trans_quotes = "'")
+  with_single <- new_gloss_data(list(ex_sp, ex_gloss), translation = ex_trans, trans_quotes = "'")
   expect_match(attr(with_single, "translation"), "^'An example in Spanish.'$")
+})
+
+test_that("Wrong output throws error", {
+  expect_error(new_gloss_data(ex_sp))
+  expect_error(gloss_df(data.frame()))
+  expect_error(gloss_df(exp_sp))
 })
