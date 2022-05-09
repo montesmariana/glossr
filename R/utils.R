6e7c23ba20173b531dcb690890867e01b1145251
mariana <mariana.montes@kuleuven.be>
2022-05-09 10:36:23 +0200

    add line styling

--- R/utils.R
+++ R/utils.R
@@ -11,6 +11,7 @@ set_default <- function(x, default = "") {
 #' Parse latex
 #'
 #' @param string Latex string to parse
+#' @param is_cell For word output, whether to style raw text or a cell
 #'
 #' @return Character vector
 #' @name parse_latex
@@ -21,7 +22,6 @@ NULL
 #' @param output Character string with output format required.
 #'
 #' @return Invisible, the output. It also sets it as the 'glossr.output' option.
-#' @export
 validate_output <- function(output = c("word", "latex", "leipzig", "tooltip", "html", "pdf")) {
   output <- tryCatch(
     error = function(cnd) {
@@ -37,4 +37,31 @@ validate_output <- function(output = c("word", "latex", "leipzig", "tooltip", "h
   if (output == "pdf") output <- "latex"
   options("glossr.output" = output)
   invisible(output)
+}
+
+#' List of styling options
+#'
+#' @param format \code{i} for italics and \code{b} for bold
+#'
+#' @return Character vector with the ways that a certain format (italics or bold) can be specified.
+style_options <- function(format = c("i", "b")) {
+  switch(
+    format,
+    i = c("i", "it", "italics", "textit"),
+    b = c("b", "bf", "bold", "textbf")
+  )
+}
+
+#' Check if required packages are installed
+#'
+#' Calls \code{\link{requireNamespace}} with the required packages.
+#'
+#' @param output_format Word, Leipzig or Tooltip, desired format
+check_packages <- function(output_format) {
+  if (output_format == "word") {
+    requireNamespace("officer", quietly = TRUE)
+    requireNamespace("flextable", quietly = TRUE)
+  } else if (output_format %in% c("leipzig", "tooltip")) {
+    requireNamespace("htmltools", quietly = TRUE)
+  }
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,25 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("defaults are set", {
+  expect_null(set_default(NULL, NULL))
+  expect_null(set_default(3, NULL))
+  expect_equal(nchar(set_default(NA)), 0)
+  expect_equal(nchar(set_default('')), 0)
+  expect_equal(nchar(set_default(NULL)), 0)
+  expect_equal(nchar(set_default(3)), 0)
+  expect_equal(set_default("test"), "test")
+  expect_equal(set_default("test", NULL), "test")
+})
+
+test_that("output is set", {
+  expect_warning(validate_output("test"))
+  expect_invisible(validate_output("latex"))
+  expect_invisible(validate_output("leipzig"))
+  expect_invisible(validate_output("html"))
+  expect_invisible(validate_output("word"))
+})
+
+test_that("style options are correct", {
+  expect_equal(style_options("i"), c("i", "it", "italics", "textit"), ignore_attr = TRUE)
+  expect_equal(style_options("b"), c("b", "bf", "bold", "textbf"), ignore_attr = TRUE)
+  expect_null(style_options("m"))
+  expect_null(style_options("bold"))
 })
